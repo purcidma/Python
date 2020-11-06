51fa4286114fa6532e2adfdb226231c3749bc46a
purcidma <72094665+purcidma@users.noreply.github.com>
2020-11-06 11:34:03 -0600

    ok

--- post2.py
+++ post2.py
@@ -1,6 +1,6 @@
 import requests
 #r = requests.post('http://bugs.python.org', data={'@number': 18061, '@type': 'issue', '@action': 'show'})
-in_values = {'number' : 18061, 'type': issue, 'action':'show'}
+in_values = {'number' : 18061, 'type': 'issue', 'action':'show'}
 r = requests.post('http://bugs.python.org',data = in_values)
 print(r.status_code, r.reason)
 
