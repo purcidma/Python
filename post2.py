1b96a69ef9a26199962b5afdcff02c28a3f11a6e
purcidma <72094665+purcidma@users.noreply.github.com>
2020-11-06 12:20:35 -0600

    update

--- post2.py
+++ post2.py
@@ -1,10 +1,3 @@
 import requests
-
-req_body = {'name': 'kyle pogi',
-                'salary': '3232323',
-                'age': '34'}
-
-response = requests.post('http://dummy.restapiexample.com/api/v1/create', json=req_body)
-
-print(response.status_code)
-print(response.json())
+response = requests.get("http://api.open-notify.org/astros.json")
+print(response)
